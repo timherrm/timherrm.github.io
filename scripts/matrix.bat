143b3e01b78a13b6acde6665a8e5e4b8543f010d
timherrm <timo.herrmann1@web.de>
2017-05-09 07:29:36 +0200

    scripts

---
+++ scripts/matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0c
+mode 1000
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
